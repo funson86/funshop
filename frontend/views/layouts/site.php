b7ce7183d84ceb40a3f8f6a31941d3fb84b2d7d2
花生 <funson86@gmail.com>
2015-09-10 14:53:38 +0800

    Update site.php
    bug fix

--- frontend/views/layouts/site.php
+++ frontend/views/layouts/site.php
@@ -21,16 +21,6 @@ SiteAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
 <body>
 <?php $this->beginBody() ?>
 <div id="header" class="new_header">
